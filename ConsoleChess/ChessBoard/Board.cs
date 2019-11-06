32a85c3e0b14caa70997c3bebe3788b7779f185f
Mauro Doneda <mauro.doneda@gmail.com>
2019-11-06 20:11:49 +0000

    Created custom exeptions and position validation on class Board

--- ConsoleChess/ChessBoard/Board.cs
+++ ConsoleChess/ChessBoard/Board.cs
@@ -24,10 +24,41 @@ namespace ConsoleChess.ChessBoard
             return Pieces [line, column];
         }
 
+        public Piece piece(Position pos)
+        {
+            return Pieces[pos.Line, pos.Column];
+        }
+
         public void putPiece(Piece p, Position pos)
         {
             Pieces[pos.Line, pos.Column] = p;
             p.Position = pos;
         }
+
+        public bool existentPiece(Position pos)
+        {
+            validatePosition(pos);
+            return piece(pos) != null;
+        }
+
+        public bool validPosition(Position pos)
+        {
+            if (pos.Line<0 || pos.Line>=Lines || pos.Column<0 || pos.Column >= Columns)
+            {
+                return false;                        
+            }
+            return true;
+        }
+
+        public void validatePosition(Position pos)
+        {
+            if (!validPosition(pos))
+            {
+                throw new BoardException("Invalid Position");
+            }
+        }
+
+
+
     }
 }
---
+++ ConsoleChess/ChessBoard/BoardException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleChess.ChessBoard
+{
+    class BoardException : Exception 
+    {
+        public BoardException(string msg): base(msg)
+        {
+        }
+    }
+
+}
