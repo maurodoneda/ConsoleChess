75fc7c8bf94c0505e89b66d38122a5d45ac0ff7a
Mauro Doneda <mauro.doneda@gmail.com>
2019-11-06 10:14:12 +0000

    Created classes Board, Color and Piece

---
+++ ConsoleChess/Board/Board.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace ConsoleChess.Board
+{
+    class Board
+    {
+        public int Lines { get; set; }
+        public int Columns { get; set; }
+
+        private Piece[,] Pieces;
+
+        public Board(int lines, int columns, Piece[,] pieces)
+        {
+            Lines = lines;
+            Columns = columns;
+            Pieces = new Piece[lines,columns];
+        }
+    }
+}
---
+++ ConsoleChess/Board/Piece.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ConsoleChess.Board
+{
+    class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; protected set; }
+        public int QntMoves { get; protected set; }
+        public Board Board { get; protected set; }
+
+        public Piece(Position position, Color color, Board board)
+        {
+            Position = position;
+            Color = color;
+            Board = board;
+            QntMoves = 0;
+        }
+    }
+}
